e3e11bab03796b753cba7f66822bcd8647401de9
Luka Kunić <Luka Kuni>
2016-12-15 14:37:23 +0100

    Initial commit

---
+++ Source/UnrealPorter/UnrealPorterGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealPorter.h"
+#include "UnrealPorterGameModeBase.h"
+
+
+
+
---
+++ Source/UnrealPorter/UnrealPorterGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UnrealPorterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPORTER_API AUnrealPorterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
